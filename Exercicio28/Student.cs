1cb8f2ee5578a7179ead312a02d32b7ea9b0bd57
Gabriel Laurindo <gabrielrodrigo380@hotmail.com>
2021-03-21 17:43:37 -0300

    Projeto faz reserva de quartos e retorna os que forao reservados

---
+++ Exercicio28/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio28
+{
+    class Student
+    {
+        public string Nome  { get; set; }
+        public string Email { get; set; }
+
+        public Student(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return Nome
+                + ", "
+                + Email;
+        }
+
+    }
+}
